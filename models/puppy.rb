8f849f3043199a7de603e24a4ce6fa77d4f98fd9
Rachel Wyatt <rw126@evansville.edu>
2018-12-23 13:53:44 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,7 @@
 class Puppy
   attr_accessor :name, :breed, :age
   @@all = []
-  
+
   def initialize(name, breed, age)
     @name = name
     @breed = breed
@@ -9,7 +9,7 @@ class Puppy
     @@all << self
   end
 
-  def self.all 
+  def self.all
     @@all
   end
 
