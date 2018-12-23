6b59235bf404737591746a093c4be5613278b9c3
Rachel Wyatt <rw126@evansville.edu>
2018-12-23 13:30:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,4 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    @puppy = Puppy.new(params[:user_text])
+    erb :results
+  end
+
 end
